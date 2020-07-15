6d019ac398f24847ce9d8416b35e3a948d7ec7a2
Yutong Han <yxh703@miami.edu>
2020-07-15 17:40:25 -0400

    Updates

---
+++ precache-manifest.b8b60c0aab84fc932297dd1ec624474f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d71282efe1fdc1553c0c97828084a3b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe5d6cfe793280b327af",
+    "url": "/monsters-rolodex/static/css/main.5156af98.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5d6cfe793280b327af",
+    "url": "/monsters-rolodex/static/js/main.dec02c54.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
